98377bbfa518059c14a94ff0a84fc60780cc1b26
Dean Dupalov <75751527+DeanDupalov@users.noreply.github.com>
2021-09-03 17:31:02 +0300

    setup

---
+++ Fundamentals/Syntax, Functions and Statements/demo.js
@@ -0,0 +1,12 @@
+function exellentGrade(grade) {
+    if (grade >= 5.50) {
+        console.log('Excellent'); 
+    } else {
+        console.log('Not excellent');
+    }
+    
+}
+
+
+
+exellentGrade(5.50);

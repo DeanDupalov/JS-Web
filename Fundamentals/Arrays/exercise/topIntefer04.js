156bd6b7eb8a9a2ded2ffa2498e8c645ac36e02e
Dean Dupalov <75751527+DeanDupalov@users.noreply.github.com>
2021-09-10 16:35:31 +0300

    new folder

--- Fundamentals/Arrays/exercise/topIntefer04.js
+++ Fundamentals/Arrays/exercise/topIntefer04.js
@@ -1,7 +1,35 @@
 function solve(numbers) {
-    let topNumIndex = 0
+    let result = [];
+    let isTop = false
 
-    
+    for (let i = 0; i < numbers.length; i++) {
+        if (isTop) {
+            break;
+        }
+        if (i === numbers.length - 1) {
+            result = [numbers[i]];
+            break;
+        }else {
+            result = [numbers[i]]
+        }
+        
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[j] >= numbers[i]) {
+                result = [numbers[j]];
+                break;
+            } else {
+                result.push(numbers[j]);
+                if (j == numbers.length-1){
+                    isTop = true
+                }
+            }
+        }
+    }
+    console.log(result.join(' '))
+
 }
 
-solve([27, 19, 42, 2, 13, 45, 48])
+solve([1, 4, 3, 2]);
+solve([14, 24, 3, 19, 15, 17]);
+solve([41, 41, 34, 20]);
+solve([27, 19, 42, 2, 13, 45, 48]);
---
+++ Fundamentals/Functions/exercise/arrowFunctions.js
@@ -0,0 +1,5 @@
+
+
+result = [1, 2, 3, 4].filter(value => value % 2 === 0);
+
+console.log(result);

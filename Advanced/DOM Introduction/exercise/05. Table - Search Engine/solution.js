b31534fcfee7589644032200dfe43b8e81b1d794
Dean Dupalov <75751527+DeanDupalov@users.noreply.github.com>
2021-09-27 20:33:12 +0100

    DOM exercise

--- Advanced/DOM Introduction/exercise/05. Table - Search Engine/solution.js
+++ Advanced/DOM Introduction/exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,18 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+
+      let input = document.getElementById('searchField').value;
+      let table = Array.from(document.querySelectorAll('tbody tr'));
+
+      for (let row of table) {
+
+         if(row.textContent.includes(input)){
+            row.classList.toggle('select');
+         }
+      }
 
    }
+ 
+
 }
--- Advanced/DOM Introduction/exercise/06. Format the Text/solution.js
+++ Advanced/DOM Introduction/exercise/06. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+  let textInput = document.getElementById('input').value.split('.').filter((el) => el != '');
+  let output = document.getElementById('output');
+  let finalString = ''
+  let text = '';
+  let count = 0;
+
+  for (let i = 0; i < textInput.length; i++) {
+    if (count === 3) {
+      count = 0;
+      finalString += `<p>${text}.</p>`;
+      text = '';
+    }
+    text += textInput[i];
+    count++;
+
+  }
+  finalString += `<p>${text}.</p>`;
+ 
+  output.innerHTML = finalString;
+
 }

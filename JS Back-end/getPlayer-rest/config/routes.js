41537768448f6a034f495e20db487740734ea76a
Dean Dupalov <75751527+DeanDupalov@users.noreply.github.com>
2022-04-17 12:57:41 +0100

    fetch sevice

--- JS Back-end/getPlayer-rest/config/routes.js
+++
@@ -1,9 +0,0 @@
-const usersController = require('../controllers/users');
-const gamesController = require('../controllers/catalog');
-const homeController = require('../controllers/home');
-
-module.exports = (app) => {
-    app.use(homeController);
-    app.use('/users', usersController);
-    app.use('/data/games', gamesController);
-}
--- JS Back-end/getPlayer-rest/controllers/home.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', (req, res) => {
-    res.json({message: 'REST service operational.'})
-});
-
-
-module.exports = router;
